3db1400e16f926ee47b555f482e051d4d41631d0
Voronok Vitaliy <vitaliy.voronok@idapgroup.com>
2015-01-05 14:10:06 +0200

    Initial Commit

---
+++ ZombieConga/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZombieConga
+//
+//  Created by Voronok Vitaliy on 1/5/15.
+//  Copyright (c) 2015 IDPGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
