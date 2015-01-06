a56c6198a47c609a1b0c45ba57e0284c8ca39803
Voronok Vitaliy <vitaliy.voronok@idapgroup.com>
2015-01-06 09:03:24 +0200

    Set position background sprite Add zombie sprite

--- ZombieConga/GameScene.m
+++ ZombieConga/GameScene.m
@@ -8,48 +8,38 @@
 
 #import "GameScene.h"
 
+@interface GameScene ()
+@property (nonatomic, strong)   SKSpriteNode    *zombie1;
+
+@end
+
 @implementation GameScene
 
 - (void)didMoveToView:(SKView *)view {
     self.backgroundColor = [SKColor whiteColor];
     SKSpriteNode *background = [[SKSpriteNode alloc] initWithImageNamed:@"background1"];
+    
+    // Set position sprite Background
+    CGSize size = self.size;
+    background.position = CGPointMake(size.width / 2, size.height / 2);
+//    background.anchorPoint = CGPointZero;
+//    background.position = CGPointZero;
+    
+//    background.zRotation = M_PI / 8;
+    
+    background.zPosition = -1;
+    
     [self addChild:background];
+    
+    // Create sprite zombie
+    SKSpriteNode *zombie1 = [[SKSpriteNode alloc] initWithImageNamed:@"zombie1"];
+    zombie1.position = CGPointMake(400, 400);
+    
+    self.zombie1 = zombie1;
+    
+    [self addChild:zombie1];
+    
+    NSLog(@"Size : (%@)", NSStringFromCGSize(background.size));
 }
-
-//-(void)didMoveToView:(SKView *)view {
-//    /* Setup your scene here */
-//    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-//    
-//    myLabel.text = @"Hello, World!";
-//    myLabel.fontSize = 65;
-//    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-//                                   CGRectGetMidY(self.frame));
-//    
-//    [self addChild:myLabel];
-//}
-//
-//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    /* Called when a touch begins */
-//    
-//    for (UITouch *touch in touches) {
-//        CGPoint location = [touch locationInNode:self];
-//        
-//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-//        
-//        sprite.xScale = 0.5;
-//        sprite.yScale = 0.5;
-//        sprite.position = location;
-//        
-//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-//        
-//        [sprite runAction:[SKAction repeatActionForever:action]];
-//        
-//        [self addChild:sprite];
-//    }
-//}
-//
-//-(void)update:(CFTimeInterval)currentTime {
-//    /* Called before each frame is rendered */
-//}
 
 @end
